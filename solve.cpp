65062f3ef0ee3a812e084c01e290fd23d951fcf1
Fan JIN <kingium@outlook.com>
2020-03-08 01:15:50 +0000

    add two-level blocking

--- solve.cpp
+++ solve.cpp
@@ -55,7 +55,12 @@ static double** mem_R_tmp;
 #ifndef FUSED
   #define FUSED 1
 #endif
-#define BLOCK_SIZE_0 50
+#ifndef BLOCK_SIZE_0
+#define BLOCK_SIZE_0 200
+#endif
+#ifndef BLOCK_SIZE_1
+#define BLOCK_SIZE_1 50
+#endif
 
 #if BLOCKING
 static inline void copy_blk_pad (double* dst, int dh, int dw, int dlda, double* src, int sh, int sw, int slda) {
@@ -69,7 +74,7 @@ static inline void copy_blk_pad (double* dst, int dh, int dw, int dlda, double* 
 #endif
 
 // do_block_0_fused(blk_E_prev_tmp, blk_R_tmp, blk_E_tmp, BLOCK_SIZE_0, BLOCK_SIZE_0, BLOCK_SIZE_0);
-static inline void do_block_0_fused(double *E_prev, double *R, double *E,
+static inline void do_block_1_fused(double *E_prev, double *R, double *E,
                                     int m, int n, int lda, double alpha, double dt) {
   const int innerBlockRowStartIndex = (n+2)+1;
   const int innerBlockRowEndIndex = (((m+2)*(n+2) - 1) - (n)) - (n+2);
@@ -84,6 +89,30 @@ static inline void do_block_0_fused(double *E_prev, double *R, double *E,
           R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_prev_tmp[i]+M2))*(-R_tmp[i]-kk*E_prev_tmp[i]*(E_prev_tmp[i]-b-1));
       }
   }
+}
+
+static inline void do_block_0_fused(double *E_prev, double *R, double *E,
+                                    int m, int n, int lda, double alpha, double dt) {
+    double * blk_E_prev_tmp = mem_E_prev_tmp[0];
+    double * blk_R_tmp = mem_R_tmp[0];
+    double * blk_E_tmp = mem_E_tmp[0];
+    for (int i=1; i<=m; i+=BLOCK_SIZE_1) {
+      for (int j=1; j<=n; j+=BLOCK_SIZE_1) {
+        const int M = min (BLOCK_SIZE_1+2, m+3-i);
+        const int N = min (BLOCK_SIZE_1+2, n+3-j);
+
+        copy_blk_pad(blk_E_prev_tmp, BLOCK_SIZE_1+2, BLOCK_SIZE_1+2, BLOCK_SIZE_1+2, E_prev + (i-1)*lda + j-1, M, N, lda);
+        copy_blk_pad(blk_R_tmp, BLOCK_SIZE_1+2, BLOCK_SIZE_1+2, BLOCK_SIZE_1+2, R + (i-1)*lda + j-1, M, N, lda);
+        // copy_blk_pad(blk_E_tmp, BLOCK_SIZE_1+2, BLOCK_SIZE_1+2, BLOCK_SIZE_1+2, E_tmp + (i-1)*lda + j-1, M, N, lda);
+
+        do_block_1_fused(blk_E_prev_tmp, blk_R_tmp, blk_E_tmp, BLOCK_SIZE_1, BLOCK_SIZE_1, BLOCK_SIZE_1+2, alpha, dt);
+
+        copy_blk_pad(E+i*lda+j, M-2, N-2, lda, blk_E_tmp+(BLOCK_SIZE_1+2)+1, M-2, N-2, BLOCK_SIZE_1+2);
+        copy_blk_pad(R+i*lda+j, M-2, N-2, lda, blk_R_tmp+(BLOCK_SIZE_1+2)+1, M-2, N-2, BLOCK_SIZE_1+2);
+        // copy_blk_pad(E_prev_tmp+i*lda+j, M-2, N-2, lda, blk_E_prev_tmp+(BLOCK_SIZE_1+2)+1, M-2, N-2, BLOCK_SIZE_1+2);
+
+      }
+    }
 }
 
 // #ifdef SSE_VEC
@@ -135,13 +164,13 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
   mem_E_prev_tmp = (double **)malloc(4*sizeof(double*));
   mem_R_tmp = (double **)malloc(4*sizeof(double*));
 
-  if (posix_memalign((void **) &mem_E_tmp[0], ALIGNMENT, (BLOCK_SIZE_0+2)*(BLOCK_SIZE_0+2)*sizeof(double)) != 0) {
+  if (posix_memalign((void **) &mem_E_tmp[0], ALIGNMENT, (BLOCK_SIZE_1+2)*(BLOCK_SIZE_1+2)*sizeof(double)) != 0) {
       printf("[ERROR] posix_memalign: failed in REDUCE_CONFLICT_3 (&mem_E_tmp)\n");
   }
-  if (posix_memalign((void **) &mem_E_prev_tmp[0], ALIGNMENT, (BLOCK_SIZE_0+2)*(BLOCK_SIZE_0+2)*sizeof(double)) != 0) {
+  if (posix_memalign((void **) &mem_E_prev_tmp[0], ALIGNMENT, (BLOCK_SIZE_1+2)*(BLOCK_SIZE_1+2)*sizeof(double)) != 0) {
       printf("[ERROR] posix_memalign: failed in REDUCE_CONFLICT_3 (&mem_E_prev_tmp)\n");
   }
-  if (posix_memalign((void **) &mem_R_tmp[0], ALIGNMENT, (BLOCK_SIZE_0+2)*(BLOCK_SIZE_0+2)*sizeof(double)) != 0) {
+  if (posix_memalign((void **) &mem_R_tmp[0], ALIGNMENT, (BLOCK_SIZE_1+2)*(BLOCK_SIZE_1+2)*sizeof(double)) != 0) {
       printf("[ERROR] posix_memalign: failed in REDUCE_CONFLICT_3 (&mem_R_tmp)\n");
   }
 #endif
@@ -256,27 +285,15 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
 
 #if FUSED
     // Solve for the excitation, a PDE
-    const int lda = n+2;
-    double * blk_E_prev_tmp = mem_E_prev_tmp[0];
-    double * blk_R_tmp = mem_R_tmp[0];
-    double * blk_E_tmp = mem_E_tmp[0];
+
 
 #if BLOCKING
     for (int i=1; i<=m; i+=BLOCK_SIZE_0) {
       for (int j=1; j<=n; j+=BLOCK_SIZE_0) {
-        const int M = min (BLOCK_SIZE_0+2, m+3-i);
-        const int N = min (BLOCK_SIZE_0+2, n+3-j);
-
-        copy_blk_pad(blk_E_prev_tmp, BLOCK_SIZE_0+2, BLOCK_SIZE_0+2, BLOCK_SIZE_0+2, E_prev + (i-1)*lda + j-1, M, N, lda);
-        copy_blk_pad(blk_R_tmp, BLOCK_SIZE_0+2, BLOCK_SIZE_0+2, BLOCK_SIZE_0+2, R + (i-1)*lda + j-1, M, N, lda);
-        // copy_blk_pad(blk_E_tmp, BLOCK_SIZE_0+2, BLOCK_SIZE_0+2, BLOCK_SIZE_0+2, E_tmp + (i-1)*lda + j-1, M, N, lda);
-
-        do_block_0_fused(blk_E_prev_tmp, blk_R_tmp, blk_E_tmp, BLOCK_SIZE_0, BLOCK_SIZE_0, BLOCK_SIZE_0+2, alpha, dt);
-
-        copy_blk_pad(E+i*lda+j, M-2, N-2, lda, blk_E_tmp+(BLOCK_SIZE_0+2)+1, M-2, N-2, BLOCK_SIZE_0+2);
-        copy_blk_pad(R+i*lda+j, M-2, N-2, lda, blk_R_tmp+(BLOCK_SIZE_0+2)+1, M-2, N-2, BLOCK_SIZE_0+2);
-        // copy_blk_pad(E_prev_tmp+i*lda+j, M-2, N-2, lda, blk_E_prev_tmp+(BLOCK_SIZE_0+2)+1, M-2, N-2, BLOCK_SIZE_0+2);
-
+        const int offset = (i-1)*(n+2)+(j-1);
+        const int M = min (BLOCK_SIZE_0, m+1-i);
+        const int N = min (BLOCK_SIZE_0, n+1-j);
+        do_block_0_fused(E_prev+offset, R+offset, E+offset, M, N, n+2, alpha, dt);
       }
     }
 #else
